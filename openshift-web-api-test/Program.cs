e5564b7dcd636e0c3fba2da1e5c04e454c17fad6
Altamir Cassiano <altamir.cassiano@energisa.com.br>
2025-04-21 18:59:24 -0300

    Add Health check

--- openshift-web-api-test/Program.cs
+++ openshift-web-api-test/Program.cs
@@ -6,6 +6,7 @@ builder.Services.AddControllers();
 // Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
 builder.Services.AddEndpointsApiExplorer();
 builder.Services.AddSwaggerGen();
+builder.Services.AddHealthChecks();
 
 var app = builder.Build();
 
@@ -15,7 +16,7 @@ var app = builder.Build();
     app.UseSwagger();
     app.UseSwaggerUI();
 //}
-
+app.MapHealthChecks("/health");
 app.UseHttpsRedirection();
 
 app.UseAuthorization();

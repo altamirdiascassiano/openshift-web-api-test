ccb96e47e3d731b308a78dd95af6ef3e0ed4b837
Altamir Cassiano <altamir.cassiano@energisa.com.br>
2025-04-21 19:22:42 -0300

    add healthz para novos testes

---
+++ openshift-web-api-test/Controllers/HealthzController.cs
@@ -0,0 +1,23 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace openshift_web_api_test.Controllers
+{
+    [ApiController]
+    [Route("[controller]")]
+    public class HealthzController : Controller
+    {
+
+        private readonly ILogger<WeatherForecastController> _logger;
+
+        public HealthzController(ILogger<WeatherForecastController> logger)
+        {
+            _logger = logger;
+        }
+
+        [HttpGet(Name = "Healthz")]
+        public ActionResult Get()
+        {
+            return Ok("I'm Working");
+        }
+    }
+}
